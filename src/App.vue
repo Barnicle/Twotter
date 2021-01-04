2fb9fd42de1b7a8850a7c96c52c5e4d2433dbd77
EGalimov <egalimov@komus.net>
2021-01-04 12:11:56 +0300

    basic vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{fullName}}
+    <span>Followers: {{followers}}</span>
+    <button v-on:click="followUser">Follow</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'App',
+  data(){
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        userName: 'Barnicle',
+        firstName: 'Eugene',
+        lastName: 'Galimov',
+        email: 'egalimov95@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.userName} gain follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  mounted() {
+    // this.followUser()
   }
 }
 </script>
@@ -21,8 +51,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-flow: column;
 }
 </style>
